2dc1e14d9f9745a91ac8138c8560ecf563373310
ongchunheng <ocheng.90@gmail.com>
2023-01-07 12:51:28 +0800

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,35 @@
 import React from "react";
+import footerLogo from "../images/footer-logo.png"
 
 export default function Footer() {
   return (
-    <footer></footer>
+    <footer>
+      <section>
+        <img src={footerLogo} alt="footer logo" className="footer-img" />
+        <section>
+          <h3>Doormat Navigation</h3>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/reservation">Reservations</a></li>
+            <li><a href="/contact">Contact Me</a></li>
+            <li><a href="/login">Login</a></li>
+          </ul>
+        </section>
+
+        <section>
+          <h3>Contact</h3>
+          <p>Address</p>
+          <p>Number</p>
+        </section>
+
+        <section>
+          <h3>Social Media Links</h3>
+          <p>Facebook</p>
+          <p>Twitter</p>
+        </section>
+      </section>
+    </footer>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,17 @@
 import React from "react";
+import restaurantfood from "../images/restauranfood.jpg";
 
 export default function Main() {
   return (
     <main>
       <section className="intro">
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        <article className="intro-article">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <button className="reserve-btn">Reserve a Table</button>
+          <img src={restaurantfood} alt="restaurant food" />
+        </article>
       </section>
     </main>
   )
