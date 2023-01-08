c9197d4356db701bcde64cc20960c352aa53c7c1
ongchunheng <ocheng.90@gmail.com>
2023-01-08 20:52:59 +0800

    added booking page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from "./components/Homepage"
 import Footer from "./components/Footer"
 import Nav from "./components/Nav"
+import BookingPage from './components/BookingPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/booking" element={<BookingPage />}></Route>
       </Routes>
       <Footer />
     </>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -32,7 +32,7 @@ export default function Nav() {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/menu">Menu</Link></li>
-          <li><Link to="/reservation">Reservations</Link></li>
+          <li><Link to="/booking">Reservations</Link></li>
           <li><Link to="/contact">Contact Me</Link></li>
           <li><Link to="/login">Login</Link></li>
         </ul>
